2992da158d34799219ccf0adc527bac1b03432c7
DandyEnders <howden2@gmail.com>
2022-04-22 15:00:47 +0900

    Props syntax usage

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,21 +13,21 @@ function App() {
   // let [title2, b2] = useState("Second best is the medium coffee.");
   // let [title3, b3] = useState("Actually, large is good enough to share!");
   let [title, setTitle] = useState(["The small is supreme.", "Medium is premium.", "Large for a share!"]);
-  let [thumbsUp, thumbsUpChange] = useState(0);
-  let [isModal, setIsModal] = useState(false);
-  
-  
+  let [thumbsUp, thumbsUpChange] = useState([0, 0, 0]);
+  let [isModal, setIsModal] = useState(true);
+
+  let teaChangerFunc = () => {
+    let temp = [...title];
+    temp[0] = "The tea is supreme.";
+    setTitle(temp);
+  };
   return (
     <div className="App" >
       <div className="black-nav">
         <h4>Blog</h4>
       </div>
 
-      <button onClick={() => {
-        let temp = [...title];
-        temp[0] = "The tea is supreme.";
-        setTitle(temp);
-      }}>For the Tea gangs!</button>
+      <button onClick={teaChangerFunc}>For the Tea gangs!</button>
 
       <button onClick={() => {
         let temp = [...title];
@@ -35,7 +35,7 @@ function App() {
         setTitle(temp);
       }}>Sort by Alphabet</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{title[0]}<span onClick={() => { thumbsUpChange(thumbsUp + 1) }}>👍</span> {thumbsUp} </h4>
         <p>April-20th</p>
       </div>
@@ -44,12 +44,27 @@ function App() {
         <p>Feb-20th</p>
       </div>
       <div className="list">
-        <h4 onClick={()=>{ setIsModal(!isModal); }}>{title[2]}</h4>
+        <h4 onClick={() => { setIsModal(!isModal); }}>{title[2]}</h4>
         <p>Jan-20th</p>
-      </div>
-      
+      </div> */}
+
       {
-        isModal === true ? <Modal/> : null
+        title.map((a, i) => {
+          return (
+            <div className="list" key={i}>
+              <h4 onClick={() => { setIsModal(!isModal) }}>{a} <span onClick={() => {
+                var temp = [...thumbsUp];
+                temp[i] = temp[i] + 1;
+                thumbsUpChange(temp);
+              }}>👍</span> {thumbsUp[i]}</h4>
+              <p>Feb-20th</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        isModal === true ? <Modal teaFunc={teaChangerFunc} title={title} /> : null
       }
 
     </div>
@@ -57,14 +72,15 @@ function App() {
 }
 
 // Frequently changed or used may be used as function
-function Modal() {
+function Modal(props) {
   return (
     <>
       <div>
-        <div className='modal'>
-          <h4>Title</h4>
+        <div className='modal' style={{ background: props.color }}>
+          <h4>{props.title[0]}</h4>
           <p>Date</p>
           <p>Details</p>
+          <button onClick={props.teaFunc}>Edit</button>
         </div>
       </div>
     </>
