6b8c8dbc39a909c9d48c5a92e70fdf281fbd879b
DandyEnders <howden2@gmail.com>
2022-04-23 14:14:18 +0900

    Added background + shoe pics

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,63 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <button type="button" class="btn btn-primary">Primary</button>
-    </div> 
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <div><h1>20% Season off</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style
+            component for calling extra attention to featured content
+            or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>{' '}
+          </p>
+        </div>
+      </div>
+      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>A shoe</h4>
+            <p>A fantastic shoe!</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>A shoe</h4>
+            <p>A red shoe. Esthetic!</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>A shoe</h4>
+            <p>I would wear this in my workout.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
