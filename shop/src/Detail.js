1b3d1b8c8f3ed1769b8edb034d093a44356d3404
DandyEnders <howden2@gmail.com>
2022-04-26 13:11:54 +0900

    useEffect practice

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import {withRouter} from 'react-router';
 import Data from './data.js';
 import styled from 'styled-components';
 
-function Detail(props) {
+let Box = styled.div`
+  padding : 20px;
+`;
 
+let Title = styled.h4`
+  font-size : 25px;
+  color : ${ props => props.color}
+`;
+
+
+
+function Detail(props) {
+  useEffect(() => {
+    console.log(111);
+      // let timer = setTimeout(()=>{console.log(12321)}, 2000)
+      let timer = setTimeout(()=>{
+        setAlert(false);
+      }, 3000);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, []);
+
+  let [alert, setAlert] = useState(true);
+  let [inputData, setInputData] = useState("");
+  
   let { id } = useParams();
   let [shoes] = useState(Data);
   let history = useHistory();
   let product = shoes.find((item)=>{
     return item.id == id;
   });
+  
 
   return <div className="container">
-    <div className="row">
+    <Box><Title className="red" color={''}>Detail</Title></Box>
+    <div>
+      {
+        alert === true 
+        ? <div>Stock is low!</div>
+        : null
+      }
+      {inputData}
+      <input onChange={(e)=>{
+        setInputData(e.target.value);
+      }} />
+
       <div className="col-md-6">
         <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width="100%" />
       </div>
